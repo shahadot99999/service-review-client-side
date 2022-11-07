06f67494ff86abb457821cfe7c147fd2a09088eb
shahadot99999 <shahadot99999@gmail.com>
2022-11-08 00:55:06 +0600

    footer and different command line install

---
+++ src/Pages/Home/Home/Shared/Footer/Footer.js
@@ -0,0 +1,41 @@
+import React from 'react';
+
+const Footer = () => {
+    return (
+        <footer className="footer p-10 bg-neutral text-neutral-content">
+            <div>
+                <span className="footer-title">Dr. KLA  </span>
+
+                <a href="/" className="link link-hover">MBBS, PhD Liver Med(Sydney), DSM(Vienna),</a>
+                <a href="/" className="link link-hover">Fellow in Liver and Gastroenterology.John Hunter University Hospital (New Castle)</a>
+                <a href="/" className="link link-hover">Member: International Liver Association Liver & Gastroenterology Specialist,Former Professor & Head of The Department, BIHS Hospital of Diabetic Association</a>
+            </div>
+            <div>
+                <span className="footer-title">Medinova</span>
+                <a href="/" className="link link-hover">Medinova Medical Services
+                    Consultation – 2</a>
+                <a href="/" className="link link-hover">Room no# 512(Lift 5)
+
+                    Opposite to University of Liberal Arts
+
+                    Satmosjid Road, Road No# 5/A, Dhanmondi, Dhaka</a>
+                <a href="/" className="link link-hover">For Serial :- Direct Desk or Mobile
+
+                    Assistant: 01911356298,
+                    01747317126,  01739093198, </a>
+
+            </div>
+            <div>
+                <span className="footer-title">Labaid Diagnostic (Kolabagan)</span>
+                <a href="/" className="link link-hover">Labaid Diagnostic (Kolabagan)
+                    House No. 66, Kolabagan, Dhanmondi, Dhaka-1205</a>
+                <a href="/" className="link link-hover">Visiting Hours: 6.00pm – 9.00pm (Thursday & Friday Closed)
+
+                    Assistant: 01855-609033</a>
+
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
