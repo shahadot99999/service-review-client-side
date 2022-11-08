5cfe97c2f3600b4b99db9c621c9e420bbf733744
shahadot99999 <shahadot99999@gmail.com>
2022-11-08 18:55:57 +0600

    Load Services fake data and display service Card

--- src/Pages/Home/Home/Home.js
+++ src/Pages/Home/Home/Home.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import About from '../About/About';
 import Banner from '../Banner/Banner';
+import Services from '../Services/Services';
+
 
 const Home = () => {
     return (
@@ -8,6 +10,8 @@ const Home = () => {
             <Banner></Banner>
             <h1 className=' text-4xl text-center'><b>Prof. Dr. Mahbub H Khan - Liver & Gastroenterology Specialist</b></h1>
             <About></About>
+            <Services></Services>
+
         </div>
     );
 };
