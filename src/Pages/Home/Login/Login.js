c2c09c56a1c82ff1b0b18c074559dc080dfc63bd
shahadot99999 <shahadot99999@gmail.com>
2022-11-08 20:38:20 +0600

    login component create and set up

---
+++ src/Pages/Home/Login/Login.js
@@ -0,0 +1,45 @@
+import React from 'react';
+import { Link } from 'react-router-dom';
+
+const handleLogin = event => {
+    event.preventDefault();
+}
+
+
+const Login = () => {
+    return (
+        <div className="hero w-full my-20">
+            <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
+                <div className="text-center lg:text-left">
+                    <h1 className="text-5xl text-center font-bold">Login Now !</h1>
+                </div>
+                <div className="card flex-shrink-0 w-full max-w-sm shadow-2xl bg-base-100 py-20">
+                    <h1 className="text-2xl text-center font-bold">Login</h1>
+                    <form onSubmit={handleLogin} className="card-body">
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Email</span>
+                            </label>
+                            <input type="text" name='email' placeholder="email" className="input input-bordered" />
+                        </div>
+                        <div className="form-control">
+                            <label className="label">
+                                <span className="label-text">Password</span>
+                            </label>
+                            <input type="password" name='password' placeholder="password" className="input input-bordered" />
+                            <label className="label">
+                                <Link>Forgot password?</Link>
+                            </label>
+                        </div>
+                        <div className="form-control mt-6">
+                            <input className="btn btn-primary" type="submit" value="Login" />
+                        </div>
+                    </form>
+
+                </div>
+            </div>
+        </div>
+    );
+};
+
+export default Login;
