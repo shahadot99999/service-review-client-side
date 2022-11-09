e5b2cc5f4f73cd9361844e973091aa554f3a1ec2
shahadot99999 <shahadot99999@gmail.com>
2022-11-09 12:53:29 +0600

    login set up with firbase intregration

--- src/Pages/Home/Login/Login.js
+++ src/Pages/Home/Login/Login.js
@@ -1,12 +1,28 @@
-import React from 'react';
+import React, { useContext } from 'react';
 import { Link } from 'react-router-dom';
+import { AuthContext } from '../../../context/AuthProvider/AuthProvider';
 
-const handleLogin = event => {
-    event.preventDefault();
-}
+
 
 
 const Login = () => {
+
+    const { login } = useContext(AuthContext);
+
+    const handleLogin = event => {
+        event.preventDefault();
+        const form = event.target;
+        const email = form.email.value;
+        const password = form.password.value;
+
+        login(email, password)
+            .then(result => {
+                const user = result.user;
+                console.log(user);
+            })
+            .then(error => console.log(error));
+    }
+
     return (
         <div className="hero w-full my-20">
             <div className="hero-content grid gap-20 md:grid-cols-2 flex-col lg:flex-row">
